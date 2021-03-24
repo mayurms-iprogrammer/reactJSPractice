a069787a5cee8b7cc24d5a31380eb34d2a8ff6e1
mayurms-iprogrammer <mayurms@iprogrammer.com>
2021-03-24 17:37:59 +0530

    section:1,2,3,4 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { render } from '@testing-library/react';
+import React, { useState } from 'react';
+import '../src/Component/Dashboard.css';
+import Dashboard from './Component/Dashboard';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: true,
+      person: [
+        { name: "aniket", age: "23" },
+        { name: "Tushar", age: "32" },
+        { name: "Shubham", age: "36" }
+
+      ]
+
+    }
+  }
+  switchNameHandler = (name) => {
+
+    let data = [
+      { name: name, age: "23" },
+      { name: "Tushar", age: "32" },
+      { name: "Shubham", age: "36" }
+    ]
+    this.setState({ person: data });
+  }
+
+
+  changedHandler = (value) => {
+
+    this.setState({
+      person: [
+        { name: "Ritesh", age: "23" },
+        { name: "aniket", age: "32" },
+        { name: value, age: "36" }
+      ]
+
+    });
+  }
+  togglePersonHandler = () => {
+   this.setState({isVisible:!this.state.isVisible});
+  }
+  render() {
+    return (
+      <div className='app-container' >
+        <h1>Hi,i'm React App</h1>
+        <button
+          onClick={this.togglePersonHandler}
+        >Switch</button>
+        {this.state.isVisible===false?null:  <div>
+          <Dashboard
+            name={this.state.person[0].name}
+            age={this.state.person[0].age}
+
+          />
+          <Dashboard
+            name={this.state.person[1].name}
+            age={this.state.person[1].age}
+            click={this.switchNameHandler.bind(this)}
+          />
+
+
+          <Dashboard
+            changedHandler={this.changedHandler.bind(this)}
+            name={this.state.person[2].name}
+            age={this.state.person[2].age}> my hobbies are : cricket
+
+          </Dashboard>
+
+
+
+
+
+        </div>
+
+        }
+        {this.state.person.map(person=>{
+          return <h5>{person.name}</h5>
+        })}
+      </div>
+    )
+  }
+
+
 }
-
 export default App;
---
+++ src/Component/RenderList.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import './Dashboard.css'
+
+const RenderList=(props) =>{
+ const [name,setName]=useState("")
+    return (
+        <div className="conatiner">
+            <h1 onClick={() => {  props.click()}
+            }>hi,{props.name} you are {props.age} years old
+                </h1>
+                <input
+                value={props.name}
+                 type="text" 
+                 onChange={(event) =>{
+                    
+                    props.changedHandler(event.target.value);
+                    console.log("here im her texting",event.target.value)}} />
+
+            <p>{props.children}</p>
+
+
+        </div>
+    )
+}
+
+export default RenderList;
